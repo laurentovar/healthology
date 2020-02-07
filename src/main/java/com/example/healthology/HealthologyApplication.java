7ddf7f1d62f479683c36feabf1c18efe0e0d3fbf
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-02-07 14:15:40 -0600

    created repo

---
+++ src/main/java/com/example/healthology/HealthologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/healthology/HealthologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
