7ebdc269664be6708bfb4fbfa21f30c5648998e6
alyssalanae <briggs.l.alyssa@gmail.com>
2020-02-10 08:08:27 -0600

    Displaying journal entries

--- healthology_seeder.sql
+++ healthology_seeder.sql
@@ -11,3 +11,11 @@ insert into users  (about_me, email, first_name, last_name, password, phone_numb
     'billyFromStrangerThings'
 
 );
+
+insert into client (agreed_to_terms, user_id)
+ VALUES
+    (1, 2);
+
+INSERT INTO journal (date, entry, rating, title, client_id)
+VALUES
+    ('2001-12-11', 'This has been an eventful week for me', 10, 'Week One', 1);
