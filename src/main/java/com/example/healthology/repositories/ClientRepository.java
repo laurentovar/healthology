0d349cac624cab11470c12b1305162c95a1836af
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-02-09 22:28:19 -0600

    added consent pg, client history pg, client contact pg

--- src/main/java/com/example/healthology/repositories/ClientRepository.java
+++ src/main/java/com/example/healthology/repositories/ClientRepository.java
@@ -1,8 +1,16 @@
 package com.example.healthology.repositories;
 
 import com.example.healthology.models.Client;
+import com.example.healthology.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    //Client client = clientDao.getClientByUserID(7)
+
+    @Query("from Client c where c.user_id = :id")
+    Client findClientByUser_id(Users id);
 
 }
