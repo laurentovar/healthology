e94df26d8c9b4df07c9224ae31d9f73618e62fc7
alyssalanae <briggs.l.alyssa@gmail.com>
2020-02-20 18:18:19 -0600

    fixed dates on journal entries

--- src/main/java/com/example/healthology/models/Journal.java
+++ src/main/java/com/example/healthology/models/Journal.java
@@ -1,15 +1,12 @@
 package com.example.healthology.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+
 import java.util.Date;
-import java.util.TimeZone;
 
 @Entity
 @Table(name = "journal")
@@ -71,14 +68,7 @@ public class Journal {
         this.rating = rating;
     }
 
-    public Date getDate() throws ParseException {
-//        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
-//        isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-//        Date formattedDate = isoFormat.parse(this.date.toString());
-
-//        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
-//        isoFormat.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
-//        String formattedDate = isoFormat.format(this.date);
+    public Date getDate() {
         return date;
     }
 
@@ -102,9 +92,4 @@ public class Journal {
         this.client = client;
     }
 
-//    public static void main(String[] args) throws ParseException {
-//        Journal j = new Journal();
-//        j.setDate(new Date());
-//        System.out.println(j.getDate());
-//    }
 }
